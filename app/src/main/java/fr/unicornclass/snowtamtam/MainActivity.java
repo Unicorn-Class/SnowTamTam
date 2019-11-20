ec700860f576b42e6c29a022f24b412fb323787a
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-11-20 10:01:13 +0100

    Homepage + Start of Show Airport Group page

--- app/src/main/java/fr/unicornclass/snowtamtam/MainActivity.java
+++ app/src/main/java/fr/unicornclass/snowtamtam/MainActivity.java
@@ -1,7 +1,12 @@
 package fr.unicornclass.snowtamtam;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton searchFab = (FloatingActionButton) findViewById(R.id.searchFab);
+        searchFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ShowAirportGroup.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/fr/unicornclass/snowtamtam/ShowAirportGroup.java
@@ -0,0 +1,39 @@
+package fr.unicornclass.snowtamtam;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import fr.unicornclass.snowtamtam.ui.main.SectionsPagerAdapter;
+
+public class ShowAirportGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_airport_group);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
