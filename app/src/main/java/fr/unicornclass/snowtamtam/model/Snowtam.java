3fdfc757ed8c8277d47c44a069d33bc2ca675412
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-12-09 13:59:45 +0100

    Updated Snowtam Class

--- app/src/main/java/fr/unicornclass/snowtamtam/model/Snowtam.java
+++ app/src/main/java/fr/unicornclass/snowtamtam/model/Snowtam.java
@@ -15,72 +15,19 @@ import fr.unicornclass.snowtamtam.R;
  */
 public class Snowtam {
     private Airport placeAirport;
-    private String ObservationDate;
-    private Runway runway;
+
+
     private ArrayList<Runway> runways = new ArrayList<>();
 
     /**
      * @param placeAirport    the localisation of the airport
-     * @param observationDate the SNOWTAM date
-     * @param runway          the runway informations
      */
-    public Snowtam(Airport placeAirport, String observationDate, Runway runway) {
+    public Snowtam(Airport placeAirport) {
         this.placeAirport = placeAirport;
-        ObservationDate = observationDate;
-        this.runway = runway;
     }
 
     public Airport getPlaceAirport() {
         return placeAirport;
-    }
-
-    public String getObservationDate(Context c) {
-        String humanReadableDate = ObservationDate.substring(2,4)+" ";
-        String month = ObservationDate.substring(0,2);
-        switch (month) {
-            case "01":
-                humanReadableDate += c.getResources().getString(R.string.month_January);
-                break;
-            case "02":
-                humanReadableDate += c.getResources().getString(R.string.month_February);
-                break;
-            case "03":
-                humanReadableDate += c.getResources().getString(R.string.month_March);
-                break;
-            case "04":
-                humanReadableDate += c.getResources().getString(R.string.month_April);
-                break;
-            case "05":
-                humanReadableDate += c.getResources().getString(R.string.month_May);
-                break;
-            case "06":
-                humanReadableDate += c.getResources().getString(R.string.month_June);
-                break;
-            case "07":
-                humanReadableDate += c.getResources().getString(R.string.month_July);
-                break;
-            case "08":
-                humanReadableDate += c.getResources().getString(R.string.month_August);
-                break;
-            case "09":
-                humanReadableDate += c.getResources().getString(R.string.month_September);
-                break;
-            case "10":
-                humanReadableDate += c.getResources().getString(R.string.month_October);
-                break;
-            case "11":
-                humanReadableDate += c.getResources().getString(R.string.month_November);
-                break;
-            case "12":
-                humanReadableDate += c.getResources().getString(R.string.month_December);
-                break;
-        }
-        humanReadableDate+=" "+c.getResources().getString(R.string.date_At)+" "+ObservationDate.substring(4,6)+"h"+ObservationDate.substring(6);
-        return humanReadableDate;
-    }
-
-    public Runway getRunway() {
-        return runway;
     }
 
     public Snowtam(String codedSnowtam, Context context) {
@@ -112,12 +59,8 @@ public class Snowtam {
             }
         }
         runwaysSnowtams.add(blocs);
-        //Log.i("NB RWays",""+runwaysSnowtams.size());
         this.placeAirport = Airport.getAirport(airportOACI,context);
-        //this.ObservationDate = blocs.get("B)");
-        //this.runway=new Runway(blocs);
         for (HashMap<String,String> blocks : runwaysSnowtams){
-            //for (String k : blocks.keySet()) Log.d("Keys in Blocs",k);
             Runway r = new Runway(blocks);
             this.runways.add(r);
         }
